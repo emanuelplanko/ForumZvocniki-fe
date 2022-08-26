1129ae2814afe93cf73b4b5345f7d8ce349d4695
emanuelplanko <planko.emanuel@gmail.com>
2022-08-27 01:19:38 +0200

    moj prvi commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Wrapper from "./components/Wrapper";
+import axios from "axios";
+import {UserDto} from "./classes/user.dto";
+import Me from "./pages/Me";
 
 function App() {
+  const [user,setUser] = useState<UserDto>(new UserDto(0,'','',''));
+
+  const currentUser = async () => {
+    try {
+      const res = await axios.get('http://localhost:8080/users/profile',
+          {withCredentials: true});
+
+      if (res.status == 200) {
+        console.log(res.data);
+        setUser(res.data);
+      }
+    }
+    catch (e) {
+
+    }
+  }
+
+  useEffect(() => {
+    currentUser();
+  },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Wrapper>
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<Home />} />
+            <Route path={'/login'} element={<Login />} />
+            <Route path={'/register'} element={<Register />} />
+            <Route path={'/create'} element={<CreatePost/>} />
+            <Route path={'/me'} element={<Me user={user}/>} />
+          </Routes>
+        </BrowserRouter>
+      </Wrapper>
   );
 }
 
